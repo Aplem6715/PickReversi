e3ba295210f955b90a511ac28a43fe734528f52d
Aplem6715 <Aplem6715@gmail.com>
2023-06-02 00:15:30 +0900

    石データ構造体作成

---
+++ src/board/stone.h
@@ -0,0 +1,17 @@
+﻿#ifndef STONE_H
+#define STONE_H
+
+#include <cstdint>
+
+struct Stone
+{
+public:
+	uint64_t own;
+	uint64_t opp;
+
+	void Swap();
+	void Update(uint64_t pos, uint64_t flips);
+	void Restore(uint64_t pos, uint64_t flips);
+};
+
+#endif

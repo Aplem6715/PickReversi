b33a22abe4243cca2ba587f3c415dc149db13a13
Aplem6715 <Aplem6715@gmail.com>
2023-06-03 16:53:32 +0900

    searchのガワ作成

---
+++ src/solver/search.h
@@ -0,0 +1,47 @@
+﻿#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <vector>
+#include "board/stone.h"
+#include "eval/evaluator.h"
+#include "eval/pos_eval.h"
+#include "search_option.h"
+
+namespace solver
+{
+    using namespace board;
+    using namespace eval;
+
+    class Searcher
+    {
+    public:
+        Searcher();
+        ~Searcher();
+
+        void Reset();
+        void Setup(SearchOption option);
+        void SetStones(stone_t own, stone_t opp);
+
+        /// @brief 最善手を検索
+        /// @return 最善手
+        Position Search();
+
+        /// @brief 着手位置を検索
+        /// @param ratio 選択確率の配列（idx0から1st率，2nd率）
+        /// @return 選択された着手位置
+        Position Search(std::vector<float> ratio);
+
+    private:
+
+    private:
+        // 盤面
+        Stone _stones[1];
+        // 評価関数
+        PositionEvaluator _eval[1];
+        // 探索深度
+        uint8_t _depth;
+        // 探索設定
+        SearchOption _option = DEFAULT_OPTION;
+    };
+}
+#endif
---
+++ src/solver/search_option.h
@@ -0,0 +1,20 @@
+﻿#ifndef SEARCH_OPTION_H
+#define SEARCH_OPTION_H
+
+#include "../const.h"
+
+namespace solver
+{
+    struct SearchOption
+    {
+        uint8_t MidDepth;
+        uint8_t EndDepth;
+    };
+
+    constexpr SearchOption DEFAULT_OPTION = {
+        .MidDepth = 8,
+        .EndDepth = 16,
+    };
+} // namespace solver
+
+#endif

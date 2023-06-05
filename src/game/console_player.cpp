5b2d7ab31054b85e26c66b78f5253af360b49531
Aplem6715 <Aplem6715@gmail.com>
2023-06-05 13:11:09 +0900

    ConsolePlayerクラスの実装

---
+++ src/game/console_player.cpp
@@ -0,0 +1,36 @@
+#include "console_player.h"
+#include "board/bit.h"
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+namespace game
+{
+    Position game::ConsolePlayer::Think(uint64_t own, uint64_t opp)
+    {
+        std::string str_pos;
+        Position pos;
+
+        while (true)
+        {
+            printf("位置を入力してください（A1～H8）:");
+            std::cin >> str_pos;
+
+            pos = board::PosIndexFromAscii(str_pos);
+
+            if (pos == Position::NoMove)
+            {
+                printf("(%c, %c) には置けません\n", str_pos[0], str_pos[1]);
+            }
+            else
+            {
+                return pos;
+            }
+        }
+    }
+
+    void game::ConsolePlayer::NotifyUndo()
+    {
+        logger_.lock()->info("入力待ち");
+        std::cin.get();
+    }
+}
--- src/game/player.h
+++ src/game/player.h
@@ -3,7 +3,13 @@
 
 #include "const.h"
 #include "util/color_helper.h"
+#include <memory>
 #include <string>
+
+namespace spdlog
+{
+    class logger;
+}
 
 namespace game
 {
@@ -11,17 +17,19 @@ namespace game
     {
     public:
         virtual Position Think(uint64_t own, uint64_t opp) = 0;
-        virtual void NotifyUndo()                          = 0;
 
+        virtual void SetLogger(std::weak_ptr<spdlog::logger> logger) { logger_ = logger; }
         virtual void SetColor(Color color) { color_ = color; }
-        virtual void OnOpponentPut(Position pos){}
+        virtual void NotifyUndo(){};
+        virtual void OnOpponentPut(Position pos) {}
 
         /// @brief プレイヤー名を取得（デフォルトは色名）
         virtual const std::string GetName() { return ColorHelper::ToString(color_); }
 
-    private:
+    protected:
         Color color_;
         std::string name_;
+        std::weak_ptr<spdlog::logger> logger_;
     };
 }
 

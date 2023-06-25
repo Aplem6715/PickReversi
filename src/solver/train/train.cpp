19d1fbe4c174811ee5ce2d7aae9c08b7479ad51d
Aplem6715 <Aplem6715@gmail.com>
2023-06-25 18:21:04 +0900

    学習データファイル列挙とログ出力

--- src/solver/train/train.cpp
+++ src/solver/train/train.cpp
@@ -6,10 +6,28 @@
 #include "pattern_trainer.h"
 #include "record_batcher.h"
 #include "train_const.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 namespace train
 {
     constexpr auto kTrainDataDir = "resource/train";
+    constexpr auto kLogDir       = "log/log.txt";
+
+    namespace fs = std::filesystem;
+
+    void getFilePaths(const std::string& dir, std::vector<std::string>* filePaths)
+    {
+        for (const auto& entry : fs::directory_iterator(dir))
+        {
+            if (fs::is_regular_file(entry.path()))
+            {
+                filePaths->push_back(entry.path().string());
+            }
+        }
+    }
 
     void Train()
     {
@@ -17,26 +35,50 @@ namespace train
         PatternTrainer trainer(&eval_);
         game::MatchBook book;
         std::vector<std::string> allFiles;
-        ReplayBuffer buffer(kBatchSize, kReplayBufferSize);
+        std::array<int, kNumPhase> trainCount = {0};
 
-        // TODO: kTrainDataDirからファイル一覧を取得してallFilesに入れる
+        auto buffer = new ReplayBuffer(kBatchSize, kReplayBufferSize);
 
+        getFilePaths(kTrainDataDir, &allFiles);
+
+        std::ofstream logfile(kLogDir, std::ios::trunc); // 追記モードでファイルを開く
         for (auto& file : allFiles)
         {
             book.ReadAscii(file);
-            buffer.Load(book);
+            buffer->Load(book);
 
+            logfile << "Read Book: " << file << std::endl;
             for (int phase = 0; phase < kNumPhase; ++phase)
             {
-                if (buffer.IsBufferFull(phase))
+                logfile << "phase " << phase << ": " << buffer->GetBatchBuffer(phase)->Size() << std::endl;
+            }
+            logfile << std::endl;
+
+            // すべてのフェーズを確認して，バッファーがいっぱいになっていたら学習
+            for (int phase = 0; phase < kNumPhase; ++phase)
+            {
+                if (buffer->IsBufferFull(phase))
                 {
-                    trainer.Run(*buffer.GetBatchBuffer(phase), kTestRatio);
-                    buffer.Clear(phase);
+                    ++trainCount[phase];
+                    logfile << "Phase " << phase << ": TrainCount = " << trainCount[phase] << std::endl;
+
+                    for (int epoch = 0; epoch < kEpoch; ++epoch)
+                    {
+                        auto mae = trainer.Run(*buffer->GetBatchBuffer(phase), kTestRatio);
+                        // Epochごとにログファイルにログを出力する
+                        logfile << "\tEpoch " << epoch << ": MAE = " << mae << std::endl;
+                    }
+                    logfile << std::endl;
+
+                    buffer->Clear(phase);
                 }
             }
-
+            logfile << std::endl;
             book.Clear();
         }
+
+        logfile.close();
+        delete buffer;
     }
 }
 
